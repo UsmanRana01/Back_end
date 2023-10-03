b93c1c0f2790bf4575af28b14dd74869ce4bb103
UsmanRana01 <ranausman4052385@gmail.com>
2023-10-03 18:36:49 +0500

    FirstLab

---
+++ firstLab.py
@@ -0,0 +1,45 @@
+# print(type("Usman"))
+# example_list =[1,"Hello",4.0,"s"]
+# print(type(example_list))
+# print(example_list[1])
+# num1=input('Enter 1st Number: ')
+# num2=input('Enter 2nd Number: ')
+# print(int(num1)+int(num2))
+# num1=eval(input('Enter 1st Number: '))
+# num2=eval(input('Enter 2nd Number: '))
+# print(num1+num2)
+
+# if statement
+
+# bill_total = eval(input("Enter tha Bill Amount : "))
+# discount1=10
+# if bill_total>100:
+#     print("Bill is greater than 100!")
+#     bill_total = bill_total - discount1
+# else:
+#     print("Bill is less than 100")
+# print("total bill : "+str(bill_total))
+
+# Switch Statement
+
+# http_status = eval(input("Enter Amount : "))
+# match http_status:
+#     case 200 | 201:
+#         print("Success")
+#     case 400:
+#         print("Not found")
+#     case 500 | 501:
+#         print("Sever Error")
+#     case _:
+#         print("Unknown")
+
+#  For loop
+
+# for i in ["ABC",1,2,3]:
+#     print("Looping ..",i)
+
+# for i in range(10):
+#     print("looping ..", i)
+
+# for i in range(0,50,10):            #range (Starting, Ending, StepSize)
+#     print("looping ..", i)
