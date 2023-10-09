cb455ee246ddec1df798d03d14169155c6113719
UsmanRana01 <ranausman4052385@gmail.com>
2023-10-09 16:07:03 +0500

    quiz01

---
+++ quiz01.py
@@ -0,0 +1,9 @@
+def get_word(sentence, n):
+    if n>0:
+        words = sentence.split()
+        if n<=len(words):
+            return (words[n-1])
+    return("")
+
+print(get_word("this is a lesson about this lists", 4))
+print (get_word("this is a lesson about this lists", -4))
