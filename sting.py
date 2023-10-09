7d6164edf1db316cbb4580ac37610abdfdb25640
UsmanRana01 <ranausman4052385@gmail.com>
2023-10-09 16:54:53 +0500

    strings

---
+++ sting.py
@@ -0,0 +1,30 @@
+
+# print("Usman".isnumeric())
+
+
+# print("1234".isnumeric())
+# x = "Usman"
+# u = "Ali"
+# print(" ".join([x,u]))
+
+# new_list=["My","Name","Usman"]
+# print(" ".join(new_list))
+
+# new_list = "This is another example"
+# print(new_list.split('a'))
+
+# print(' the '.strip())
+
+# name = input("Enter a Name: ")
+# num = len(name) * 3
+# print ("Hello {num}, your lucky number is {name}".format(name=name, num=num))
+
+# price = 7.5
+# with_tax = price*1.09
+# print (price, with_tax)
+# print("Base Price: ${:.2f}, With Tax: ${:.2f}".format(price, with_tax))
+
+# def to_celsius(x):
+#     return(x-32)*5/9
+# for x in range(0,101,10):
+#     print("{:>3} F | {:>6.2f} C".format(x, to_celsius(x)))
